e60ae20a9332af8183a2af4772a78794cde3a0a2
mateuszherczka <mateuszherczka@gmail.com>
2014-08-15 16:53:19 +0200

    rewriting to c++03

---
+++ include/kuros.h
@@ -0,0 +1,10 @@
+#ifndef KUROS_H_INCLUDED
+#define KUROS_H_INCLUDED
+
+#include <constants.hpp>
+#include <types.hpp>
+#include <Server.hpp>
+#include <BlockingServer.hpp>
+
+
+#endif // KUROS_H_INCLUDED
